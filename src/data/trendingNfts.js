a4bc94b5a25e99583b6446222eec313183f421c2
Nathan Costa <nkyucode@gmail.com>
2023-09-19 15:17:11 -0300

    ✨ feat and 🐛 fix: replacing images that had loading errors and adding 2 new NFTs

--- src/data/trendingNfts.js
+++ src/data/trendingNfts.js
@@ -1,57 +1,74 @@
 export const TRENDING_NFTS = [
     {
         id: 1,
-        banner: "https://uploaddeimagens.com.br/images/004/323/805/full/nft-2.jpg?1675266899",
-        user_handle: "1A82F61",
-        user_name: "Catman",
-        user_photo: "https://i.seadn.io/gcs/files/07db5109f5e024b69237db743d5a30bf.gif?auto=format&w=256",
-        price: "3.9 ETH"
-
+        banner: "https://iili.io/JJHx0Yl.jpg",
+        user_handle: "322215",
+        user_name: "CloneY #12882",
+        user_photo:
+            "https://i.seadn.io/gcs/files/07db5109f5e024b69237db743d5a30bf.gif?auto=format&w=256",
+        price: "1.42 ETH",
     },
     {
         id: 2,
-        banner: "https://uploaddeimagens.com.br/images/004/323/807/full/nft-5.jpg?1675266920",
+        banner: "https://iili.io/JJHxqBa.jpg",
         user_handle: "pumpsolana",
-        user_name: "Blue Bear",
-        user_photo: "https://i.seadn.io/gcs/files/3c58ab27da307982a5dbe217ec6c3e9c.jpg?auto=format&w=256",
-        price: "1.72 ETH"
-
+        user_name: "Clowmig #0270",
+        user_photo:
+            "https://i.seadn.io/gcs/files/3c58ab27da307982a5dbe217ec6c3e9c.jpg?auto=format&w=256",
+        price: "0.09 ETH",
     },
     {
         id: 3,
-        banner: "https://uploaddeimagens.com.br/images/004/323/814/full/nft-20.jpg?1675266954",
-        user_handle: "pjcurly",
-        user_name: "Corgi in space ",
-        user_photo: "https://i.seadn.io/gcs/files/a55f9d8aab226d601874bf7593649549.png?auto=format&w=384",
-        price: "126 ETH"
-
+        banner: "https://iili.io/JJHxAhX.jpg",
+        user_handle: "batata2023",
+        user_name: "Asuki #10048",
+        user_photo:
+            "https://i.seadn.io/gcs/files/a55f9d8aab226d601874bf7593649549.png?auto=format&w=384",
+        price: "6.3 ETH",
     },
     {
         id: 4,
-        banner: "https://uploaddeimagens.com.br/images/004/323/816/full/nft-30.png?1675266969",
+        banner: "https://iili.io/JJHxBEJ.jpg",
         user_handle: "dogetothemoon",
-        user_name: "Golden Tea ",
-        user_photo: "https://i.seadn.io/gae/Ojd-1eIdRRgEBR5rbNWSa1vOUlqSXZKygh3_9lF8CqGI2Sddwyt-FBFNavf3zkN9L9iypwinXsv5TsvH6ADMEwClWYso8aLyOkLd7A?auto=format&w=384",
-        price: "73.2 ETH"
-
+        user_name: "BoredC #01",
+        user_photo:
+            "https://i.seadn.io/gae/Ojd-1eIdRRgEBR5rbNWSa1vOUlqSXZKygh3_9lF8CqGI2Sddwyt-FBFNavf3zkN9L9iypwinXsv5TsvH6ADMEwClWYso8aLyOkLd7A?auto=format&w=384",
+        price: "200 ETH",
     },
     {
         id: 5,
-        banner: "https://uploaddeimagens.com.br/images/004/323/810/full/gif-4.gif?1675266930",
+        banner: "https://iili.io/JJHxa4f.jpg",
         user_handle: "axieplayer99",
-        user_name: "Cute spitz",
-        user_photo: "https://i.seadn.io/gcs/files/6be057dcf15feb8ebcd81ad471379fb2.png?auto=format&w=1000",
-        price: "19.7 ETH"
-
+        user_name: "CloneY #18449",
+        user_photo:
+            "https://i.seadn.io/gcs/files/6be057dcf15feb8ebcd81ad471379fb2.png?auto=format&w=1000",
+        price: "1.92 ETH",
     },
     {
         id: 6,
-        banner: "https://uploaddeimagens.com.br/images/004/323/809/full/nft-6.jpg?1675266924",
+        banner: "https://iili.io/JJHxFhF.jpg",
         user_handle: "hireme",
-        user_name: "Polar Bear",
-        user_photo: "https://i.seadn.io/gcs/files/11d0918a25f6afd4d609e0e25d418cbd.png?auto=format&w=384",
-        price: "0.7 ETH"
-
-    }
-    
-]
+        user_name: "BoredC #2450",
+        user_photo:
+            "https://i.seadn.io/gcs/files/11d0918a25f6afd4d609e0e25d418cbd.png?auto=format&w=384",
+        price: "73.2 ETH",
+    },
+    {
+        id: 7,
+        banner: "https://iili.io/JJHxuIt.jpg",
+        user_handle: "SkyCryptoBoomer",
+        user_name: "Asuki #60032",
+        user_photo:
+            "https://i.seadn.io/gcs/files/d0e25797606242d79cc3a4f0637e4382.png?auto=format&dpr=1&w=48",
+        price: "7 ETH",
+    },
+    {
+        id: 8,
+        banner: "https://iili.io/JJHxo2R.jpg",
+        user_handle: "CryptoHolder",
+        user_name: "Clowmig #0273",
+        user_photo:
+            "https://i.seadn.io/gcs/files/376d4a73e97bf8df3828821954059bc0.png?auto=format&dpr=1&w=256",
+        price: "0.13 ETH",
+    },
+];
